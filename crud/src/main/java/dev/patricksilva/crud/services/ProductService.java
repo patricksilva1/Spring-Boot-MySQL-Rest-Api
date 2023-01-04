c8dfb1f691fa292374351d2e37ffde833bbee3ce
patricksilva1 <patricksilva5160@gmail.com>
2023-01-04 12:07:42 -0300

    feat: :sparkles: Adding ModelMapper mvnRepository, Creating findAll Products method.

--- crud/src/main/java/dev/patricksilva/crud/services/ProductService.java
+++ crud/src/main/java/dev/patricksilva/crud/services/ProductService.java
@@ -1,5 +1,30 @@
 package dev.patricksilva.crud.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dev.patricksilva.crud.model.Product;
+import dev.patricksilva.crud.repository.ProductRepository;
+import dev.patricksilva.crud.shared.ProductDTO;
+
+@Service
 public class ProductService {
-    
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<ProductDTO> findAll() {
+
+        List<Product> products = productRepository.findAll();
+
+        return products.stream()
+                .map(product -> new ModelMapper()
+                .map(product, ProductDTO.class))
+                .collect(Collectors.toList());
+    }
+
 }
