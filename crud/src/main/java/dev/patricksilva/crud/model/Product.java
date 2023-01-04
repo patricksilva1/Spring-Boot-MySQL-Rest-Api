0ca16d7d4a160b7e901b97280a28c5e6b162fbd9
patricksilva1 <patricksilva5160@gmail.com>
2023-01-03 21:26:23 -0300

    feat: :sparkles: Product created

--- crud/src/main/java/dev/patricksilva/crud/model/Product.java
+++ crud/src/main/java/dev/patricksilva/crud/model/Product.java
@@ -1,5 +1,60 @@
 package dev.patricksilva.crud.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Product {
-    
+    private Long id;
+    private String name;
+    private Integer quantity;
+    private Double price;
+    private String observation;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    // #region Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getObservation() {
+        return observation;
+    }
+
+    public void setObservation(String observation) {
+        this.observation = observation;
+    }
+
+    // #endregion
 }
